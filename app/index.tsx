798723dbc8b0b7747cda8943cbd7fefc70663be9
Fırat Batar <firat.o.batar@gmail.com>
2024-12-12 19:18:38 +0300

    Add Login and Register pages (#3)
    Add login and register page and Linter.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,45 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Pressable } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <View style={styles.container}>
+      <View>
+
+        <Link href="/login" asChild>
+          <Pressable style={styles.btn}>
+            <Text>Login</Text>
+          </Pressable>
+        </Link>
+
+        <Link href="/register" asChild>
+          <Pressable style={styles.btn}>
+            <Text>Register</Text>
+          </Pressable>
+        </Link>
+      </View>
+    </View> 
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btn: {
+    padding: 10,
+    backgroundColor: "lightgreen",
+    color: "black",
+    
+    borderWidth: 2,
+    borderRadius: 5,
+    margin: 10,
+
+    textAlign: "center",
+    alignItems: "center",
+
+    width: 200,
+  },
+});
---
+++ app/register.tsx
@@ -0,0 +1,122 @@
+import { useState } from "react";
+import { Text, View, StyleSheet, Pressable, TextInput } from "react-native";
+import Config from "./config"
+
+export default function Register() {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  function handleRegister() {
+    const requestOptions = {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(
+        {
+          email: email,
+          name: name,
+          lastname: lastName,
+          password: password
+        }
+      ),
+    }
+
+    fetch(Config.API_URL + "/register", requestOptions)
+      .then((response) => response.text())
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => console.error(error));
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Register</Text>
+      
+      <View 
+        style={{
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: 10,
+            }}
+          >
+            <TextInput
+              style={[styles.input, {width: 145}]} 
+              placeholder="Name"
+              value={name}
+              onChangeText={setName}
+            />
+            <TextInput
+              style={[styles.input, {width: 145}]} 
+              placeholder="Last Name"
+              value={lastName}
+              onChangeText={setLastName}
+            />
+          </View>
+          
+          <TextInput
+            style={styles.input} 
+            placeholder="Email"
+            value={email}
+            onChangeText={setEmail}
+          />
+          <TextInput 
+            style={styles.input}
+            placeholder="Password"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry={true}
+          />
+
+        <Pressable style={styles.btn} onPress={handleRegister}>
+          <Text>Register</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btn: {
+    padding: 10,
+    backgroundColor: "lightgreen",
+    color: "black",
+    
+    borderWidth: 2,
+    borderRadius: 5,
+    margin: 10,
+
+    textAlign: "center",
+    alignItems: "center",
+
+    width: 200,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  input: {
+    height: 50,
+    width: 300,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20,
+  },
+});
