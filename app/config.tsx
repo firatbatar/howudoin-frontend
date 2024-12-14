05247c53a21f219fef5086eb275f3739da01c682
Fırat Batar <firat.o.batar@gmail.com>
2024-12-14 13:00:08 +0300

    Update config

--- app/config.tsx
+++ app/config.tsx
@@ -1,5 +1,5 @@
 class Config {
-  public static readonly API_URL = 'http://10.51.97.53:8080';
+  public static readonly API_URL = 'http://10.51.126.222:8080';
   public static token: string = '';
 }
 
