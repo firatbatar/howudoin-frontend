e80846bfcc7659d710dd15c37d71b39959e189fd
Fırat Batar <firat.o.batar@gmail.com>
2024-12-23 11:31:14 +0300

    Add logout buttons

--- app/(tabs)/groups/_layout.tsx
+++ app/(tabs)/groups/_layout.tsx
@@ -1,13 +1,28 @@
-import { Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import React from 'react';
+import { Pressable } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import Config from '@/components/common/config';
 
 export default function GroupsLayout() {
+  const router = useRouter();
+
   return (
     <Stack>
       <Stack.Screen
         name='index'
         options={{
           title: 'Howudoin - Groups',
+          headerRight: () => (
+            <Pressable
+              onPress={() => {
+                Config.token = '';
+                router.replace('/');
+              }}
+            >
+              <MaterialIcons name="logout" size={24} color="black" />
+            </Pressable>
+          ),
         }}
       />
       <Stack.Screen
